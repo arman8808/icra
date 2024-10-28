9c4415a9fe0de772fdc55645623d248a24786247
arman8808 <armanal3066@gmail.com>
2024-10-28 18:49:36 +0530

    commit

--- src/Pages/Teams/Teams.js
+++ src/Pages/Teams/Teams.js
@@ -2,6 +2,8 @@ import React from "react";
 import TemsCard from "../../Components/TemsCard/TemsCard";
 import line from "../../Assests/Images/Group 65.png";
 import SecondTeamCard from "../../Components/TemsCard/SecondTeamCard";
+import FadeInSection from "../../Utils/FadeInSection/FadeInSection ";
+import GetInTouchCard from "../../Components/GetInTouchCard/GetInTouchCard";
 function Teams() {
   return (
     <div className="w-full flex items-center justify-center flex-col gap-[2rem]  pt-4">
@@ -17,12 +19,36 @@ function Teams() {
       <div className="w-[90%] mobile:w-[90%] tablet:w-[90%]  flex items-start justify-start flex-col gap-4  px-4 gap-4">
         <h2 className=" text-2xl font-semibold">Management</h2>
         <div className="grid grid-cols-3 gap-4 mb-2">
-      
           <SecondTeamCard />
           <SecondTeamCard />
           <SecondTeamCard />
         </div>
         <img src={line} alt="line" />
+      </div>{" "}
+      <div className="w-[90%] mobile:w-[90%] tablet:w-[90%]  flex items-start justify-start flex-col gap-4  px-4 gap-4">
+        <h2 className=" text-2xl font-semibold">Additional Directors</h2>
+        <div className="grid grid-cols-3 gap-4 mb-2">
+          <SecondTeamCard />
+          <SecondTeamCard />
+          <SecondTeamCard />
+        </div>
+        <img src={line} alt="line" />
+      </div>{" "}
+      <div className="w-[90%] mobile:w-[90%] tablet:w-[90%]  flex items-start justify-start flex-col gap-4  px-4 gap-4">
+        <h2 className=" text-2xl font-semibold">Advisory Board </h2>
+        <div className="grid grid-cols-3 gap-4 mb-2">
+          <SecondTeamCard />
+          <SecondTeamCard />
+          <SecondTeamCard />
+        </div>
+        <img src={line} alt="line" />
+      </div>
+      <div className="w-full flex items-center justify-center flex-col gap-[3rem] py-4 pt-[3rem]">
+        <FadeInSection>
+          <div className="w-[80%] mobile:w-[90%]">
+            <GetInTouchCard />
+          </div>
+        </FadeInSection>
       </div>
     </div>
   );
